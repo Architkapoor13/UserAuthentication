24f880e57256cfbe1a7c33c956d49c134075ae6c
Archit Kapoor <architkapoor13@gmail.com>
2025-03-18 19:57:14 +0530

    project initialized

---
+++ src/test/java/com/auth/userAuth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.userAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
